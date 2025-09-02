a837f8767dbee6a182c809b7974b0bcdfe149022
jorditosa <jordisato88@gmail.com>
2025-09-02 17:28:43 +0200

    shop ok

--- src/pages/shop/ShopPage.tsx
+++ src/pages/shop/ShopPage.tsx
@@ -1,22 +1,143 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonCard, IonImg } from '@ionic/react';
+import PrivateLayout from '../PrivateLayout';
+import { ShoppingCart } from 'lucide-react';
+import Heading from '../../components/ui/Heading';
 
 const ShopPage: React.FC = () => {
   return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab 3</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 3</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <h1>Botiga</h1>
-      </IonContent>
-    </IonPage>
+    <PrivateLayout
+      contentColor='light'
+    >
+
+      <main className='container'>
+        <Heading
+          title='La Botiga'
+          variant='h1'
+        />
+        <div className="grid grid-cols-2 gap-2">
+
+          <IonCard color='secondary' className='h-[240px]'>
+            <p className='text-white text-base text-center py-2'>Samarreta Dona</p>
+            <div>
+              <IonImg
+                src='/shop/samarreta_dona.jpeg'
+                className='h-40 object-cover object-bottom'
+              />
+              <IonButton
+              color='light'
+              expand='full'
+              onClick={() => window.open('https://deporunners.cat/producto/samarreta-tecnica-dona-tuga-2025/', '_blank')}
+              >
+                <ShoppingCart className='size-6 text-secondary' />
+              </IonButton>
+            </div>
+          </IonCard>
+
+          <IonCard color='secondary' className='h-[240px]'>
+            <p className='text-white text-base text-center py-2'>Samarreta Home</p>
+            <div>
+              <IonImg
+                src='/shop/samarreta.jpeg'
+                className='h-40 object-cover object-bottom'
+              />
+              <IonButton
+              color='light'
+              expand='full'
+              onClick={() => window.open('https://deporunners.cat/producto/samarreta-tecnica-home-tuga-2025-2/', '_blank')}
+              >
+                <ShoppingCart className='size-6 text-secondary' />
+              </IonButton>
+            </div>
+          </IonCard>
+
+          <IonCard color='secondary' className='h-[240px]'>
+            <p className='text-white text-base text-center py-2'>Pantaló Trail</p>
+            <div>
+              <IonImg
+                src='/shop/pantalo_trail.jpeg'
+                className='h-40 object-cover object-bottom'
+              />
+              <IonButton
+              color='light'
+              expand='full'
+              onClick={() => window.open('https://deporunners.cat/producto/samarreta-tecnica-tirants-home-tuga-2025/', '_blank')}
+              >
+                <ShoppingCart className='size-6 text-secondary' />
+              </IonButton>
+            </div>
+          </IonCard>
+
+          <IonCard color='secondary' className='h-[240px]'>
+            <p className='text-white text-base text-center py-2'>Pantaló Running</p>
+            <div>
+              <IonImg
+                src='/shop/pantalo_running.jpeg'
+                className='h-40 object-cover object-bottom'
+              />
+              <IonButton
+              color='light'
+              expand='full'
+              onClick={() => window.open('https://deporunners.cat/producto/pantalo-running-tuga-2025/', '_blank')}
+              >
+                <ShoppingCart className='size-6 text-secondary' />
+              </IonButton>
+            </div>
+          </IonCard>
+
+          <IonCard color='secondary' className='h-[240px]'>
+            <p className='text-white text-base text-center py-2'>Tallavents</p>
+            <div>
+              <IonImg
+                src='/shop/tallavents.jpeg'
+                className='h-40 object-cover object-bottom'
+              />
+              <IonButton
+              color='light'
+              expand='full'
+              onClick={() => window.open('https://deporunners.cat/producto/samarreta-tecnica-home-tuga-2025/', '_blank')}
+              >
+                <ShoppingCart className='size-6 text-secondary' />
+              </IonButton>
+            </div>
+          </IonCard>
+
+          <IonCard color='secondary' className='h-[240px]'>
+            <p className='text-white text-base text-center py-2'>Tirants Home</p>
+            <div>
+              <IonImg
+                src='/shop/tirants_home.jpeg'
+                className='h-40 object-cover object-bottom'
+              />
+              <IonButton
+              color='light'
+              expand='full'
+              onClick={() => window.open('https://deporunners.cat/producto/samarreta-tecnica-tirants-home-tuga-2025-2/', '_blank')}
+              >
+                <ShoppingCart className='size-6 text-secondary' />
+              </IonButton>
+            </div>
+          </IonCard>
+
+          <IonCard color='secondary' className='h-[240px]'>
+            <p className='text-white text-base text-center py-2'>Tirants Dona</p>
+            <div>
+              <IonImg
+                src='/shop/tirants_dona.jpeg'
+                className='h-40 object-cover object-bottom'
+              />
+              <IonButton
+              color='light'
+              expand='full'
+              onClick={() => window.open('https://deporunners.cat/producto/samarreta-tecnica-tirants-dona-tuga-2025/', '_blank')}
+              >
+                <ShoppingCart className='size-6 text-secondary' />
+              </IonButton>
+            </div>
+          </IonCard>
+         
+        </div>
+      </main>
+    </PrivateLayout>
   );
 };
 
